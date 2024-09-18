26c6da70490e12cb2b19c7f9d37eff8ed9ad6ac0
Ronaldo Aparecido da Silva <ronaldoapsilva_@hotmail.com>
2024-09-18 07:57:41 -0300

    Store Catalog | Testando facade para verificação de stock

--- src/modules/product-adm/facade/product-adm.facade.spec.ts
+++ src/modules/product-adm/facade/product-adm.facade.spec.ts
@@ -8,7 +8,7 @@ import ProductAdmFacadeFactory from "../factory/facade.factory";
 
 describe("ProductAdmFacade test", () => {
   let sequelize: Sequelize;
-  
+
   beforeEach(async () => {
     sequelize = new Sequelize({
       dialect: "sqlite",
@@ -52,4 +52,24 @@ describe("ProductAdmFacade test", () => {
     expect(product.purchasePrice).toEqual(input.purchasePrice);
     expect(product.stock).toEqual(input.stock);
   });
+
+  it("should check product stock", async () => {
+    const productFacade = ProductAdmFacadeFactory.create();
+
+    const input = {
+      id: "1",
+      name: "Product 1",
+      description: "Product 1 description",
+      purchasePrice: 10,
+      stock: 10,
+    };
+
+    await productFacade.addProduct(input);
+
+    const result = await productFacade.checkStock({ productId: "1" });
+
+    expect(result.productId).toEqual(input.id);
+    expect(result.stock).toEqual(input.stock);
+  });
+
 });
