a742ab999cd0baf8974e5cc56b0548dadb187b33
Ronaldo Aparecido da Silva <ronaldoapsilva_@hotmail.com>
2024-09-24 06:47:51 -0300

    ClientAdm | AddClientUsecase

---
+++ src/modules/client-admin/usercase/add-client/add-client.usecase.spec.ts
@@ -0,0 +1,30 @@
+import AddClientUsecase from "./add-client.ucsecase";
+
+const MockRepository = () => {
+  return {
+    add: jest.fn(),
+    find: jest.fn(),
+  };
+}
+
+describe("add Client Usecase unit test", () => {
+  it("should add a client", async () => {
+    const respository = MockRepository();
+    const usercase = new AddClientUsecase(respository);
+
+    const input = {
+      // id: "1",
+      name: "Client 1",
+      email: "x@x.com",
+      address: "Address 1",
+    };
+
+    const result = await usercase.execute(input);
+    
+    expect(respository.add).toHaveBeenCalled();
+    expect(result.id).toBeDefined();
+    expect(result.name).toEqual(input.name);
+    expect(result.email).toEqual(input.email);
+    expect(result.address).toEqual(input.address);
+  })
+})
