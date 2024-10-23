3ebf45af5e6e385c52e036649c17feba4e8588d6
Ronaldo Aparecido da Silva <ronaldoapsilva_@hotmail.com>
2024-10-23 00:37:37 -0300

    Checkout  | validateProducts

--- src/modules/checkout/usecase/place-order/place-order.usecase.ts
+++ src/modules/checkout/usecase/place-order/place-order.usecase.ts
@@ -1,12 +1,18 @@
 import UseCaseInterface from "../../../@shared/domain/usecase/use-case.interface";
 import ClientAdmFacadeInterface from "../../../client-admin/facade/client-adm.facade.interface";
+import ProductAdmFacadeInterface from "../../../product-adm/facade/product-adm.facade.interface";
 import { PlaceOrderInputDto, PlaceOrderOutputDto } from "./place-order.dto";
 
 export default class PlaceOrderUseCase implements UseCaseInterface {
   private _clientFacade: ClientAdmFacadeInterface;
+  private _productFacade: ProductAdmFacadeInterface;
 
-  constructor(clientFacade: ClientAdmFacadeInterface) {
+  constructor(
+    clientFacade: ClientAdmFacadeInterface,
+    productFacade: ProductAdmFacadeInterface
+  ) {
     this._clientFacade = clientFacade;
+    this._productFacade = productFacade;
   }
 
   async execute(input: PlaceOrderInputDto): Promise<PlaceOrderOutputDto> {
@@ -15,7 +21,8 @@ export default class PlaceOrderUseCase implements UseCaseInterface {
     if (!client) {
       throw new Error("Client not found");
     }
-    // validar produto
+
+    await this.validateProducts(input);
     // recuperar os produtos
 
     // criar o objeto do client 
@@ -34,5 +41,22 @@ export default class PlaceOrderUseCase implements UseCaseInterface {
       total: 0,
       products: []
     };
+  }
+
+  private async validateProducts(input: PlaceOrderInputDto): Promise<void> {
+    if (input.products.length === 0) {
+      throw new Error("No Products selected");
+    }
+
+    for (const p of input.products) {
+      const product = await this._productFacade.checkStock({
+        productId: p.productId
+      });
+      if (product.stock <= 0) {
+        throw new Error(
+          `Product ${product.productId} is not available in stock`
+        );
+      }
+    }
   }
 }
